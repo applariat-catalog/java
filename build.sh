5be3a13044d8722b34de90d17e41a0b059e9bc64
pouya <pouya@applariat.com>
2017-06-21 14:09:45 -0700

    using code/ instead of src/

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 # appLariats generic tomcat build script
-# Requirements - A valid jar file exists within the src_dir
-# Copies the jar file from the src_dir and copies it into /usr/src/app/
+# Requirements - A valid jar file exists within the code_dir
+# Copies the jar file from the code_dir and copies it into /usr/src/app/
 
 #Log everything in /src/build.log
-logfile=/src/build.log
+logfile=/code/build.log
 exec > $logfile 2>&1
 set -x
 
 mkdir -p /usr/src/app
 
-if ls /src/*.jar 1> /dev/null 2>&1
+if ls /code/*.jar 1> /dev/null 2>&1
 then
- cp -f /src/*.jar /usr/src/app/
+ cp -f /code/*.jar /usr/src/app/
 else
  echo "ERROR! did not find any *.jar file"
  exit 1
